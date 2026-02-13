11096e86bac3082e979ae84d3b7f5a8c037d8dfa
ELIE-BIGPAY\USER <elie@big-pay.com>
2026-02-13 22:13:35 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://localhost:8081/api',
+  apiUrl: 'https://hunting-store-backend-production.up.railway.app/api',
   stripePublishableKey: 'pk_test_51SlQYVE2WWYBB93Dn2W4f42f5qcaAfVbLjFG9UruGimAOi2osgYpoQP72lrFX2sFFhBtIFmQhDkloM9lZnJ9OgTp008gsoPFev'
 };
